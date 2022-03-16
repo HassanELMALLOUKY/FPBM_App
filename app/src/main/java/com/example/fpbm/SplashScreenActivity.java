e19c67a31b769a443a328f594e41d6c15d61b44c
HassanELMALLOUKY <hassan.elmallouky@usms.ac.ma>
2022-03-16 10:02:15 +0000

    first commit

--- app/src/main/java/com/example/fpbm/SplashScreenActivity.java
+++ app/src/main/java/com/example/fpbm/SplashScreenActivity.java
@@ -16,9 +16,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-        buttonStart = findViewById(R.id.butwelcome);
-
+        buttonStart =(Button) findViewById(R.id.butwelcome);
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

207d0f9cb324f45c1ece0f4e833e87100a67a42b
HassanELMALLOUKY <hassan.elmallouky@usms.ac.ma>
2022-04-16 10:51:40 +0000

    yarbi salama

--- app/src/main/java/com/example/fpbm/SupportFragment.java
+++ app/src/main/java/com/example/fpbm/SupportFragment.java
@@ -1,5 +1,6 @@
 package com.example.fpbm;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class SupportFragment extends Fragment {
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_support, container, false);
     }
 }

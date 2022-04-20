c881749617eae02550e167e45c6654bc17397413
HassanELMALLOUKY <hassan.elmallouky@usms.ac.ma>
2022-04-20 15:05:08 +0000

    yarbi salama 6

--- app/src/main/java/com/example/fpbm/SupportFragment.java
+++ app/src/main/java/com/example/fpbm/SupportFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,6 +19,10 @@ public class SupportFragment extends Fragment {
     View view;
     Button button;
     FirebaseAuth mFirebaseAuth;
+    TextView fullname;
+    TextView cne;
+    TextView birthday;
+    TextView filiere;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -25,6 +30,10 @@ public class SupportFragment extends Fragment {
         view=inflater.inflate(R.layout.fragment_support, container, false);
         button=view.findViewById(R.id.logout);
         mFirebaseAuth=FirebaseAuth.getInstance();
+        fullname=view.findViewById(R.id.fullname);
+        cne=view.findViewById(R.id.cne);
+        birthday=view.findViewById(R.id.birthday);
+        filiere=view.findViewById(R.id.filiere);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
